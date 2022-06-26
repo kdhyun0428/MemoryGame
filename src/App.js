6c71fc23f4a7693972991f05fb9de6285e086891
kdhyun0428 <kdhyun0428@gmail.com>
2022-06-26 09:57:43 +0900

    카드선택 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -27,8 +27,30 @@ function App() {
       setTurns(0)
   }
 
+  //카드 선택
   const handleChoice = (card) => {
-    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //두 카드를 비교
+  useEffect(() => {
+    if (choiceOne && choiceTwo){
+
+      if(choiceOne.src === choiceTwo.src){
+        console.log("일치합니다")
+        resetTurn()
+      } else{
+        console.log("불일치합니다")
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  //선택 초기화 & 턴 증가
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
   return (
